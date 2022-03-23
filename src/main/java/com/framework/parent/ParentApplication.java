0bbfa327a8ab7b1324b8ce02be8201af840f66e5
luodong <luodong0504918>
2022-03-23 16:22:46 +0800

    first commit

---
+++ src/main/java/com/framework/parent/ParentApplication.java
@@ -0,0 +1,16 @@
+package com.framework.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luodong
+ */
+@SpringBootApplication
+public class ParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParentApplication.class, args);
+    }
+
+}
